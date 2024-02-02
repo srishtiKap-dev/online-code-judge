596e5b95fb3b54129fb425c87d0848ebf69e6d5a
srishtikapoor <srishtikapoor697@gmail.com>
2024-02-03 00:28:38 +0530

    updated register API to store data in DB

--- AUTH/backend/index.js
+++ AUTH/backend/index.js
@@ -4,6 +4,8 @@ const { DBConnection } = require("./database/db");
 const { config } = require("dotenv");
 require("dotenv").config();
 const PORT = process.env.PORT || config.env.PORT;
+const User = require("./model/User");
+const bcrypt = require("bcryptjs");
 
 DBConnection();
 app.get("/", (req, res) => {
@@ -11,8 +13,46 @@ app.get("/", (req, res) => {
 });
 
 // register API
-app.post("/register", (req, res) => {
-  res.send("Register Page");
+app.post("/register", async (req, res) => {
+  try {
+    // get all data from frontend
+    const { firstname, lastname, username, password } = req.body;
+
+    // validate ALL the data should exists
+    if (!firstname && !lastname && !username && !password) {
+      return res.status(400).send("Please enter all the required details");
+    }
+
+    // validate if user already exists or not
+    const doesUserExists = await User.findOne({ username });
+    if (doesUserExists) {
+      return res.status(200).send(`User ${username} already exists`);
+    }
+    // encrypt the password
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    // save data in DB
+    const userData = await User.create({
+      firstname,
+      lastname,
+      username,
+      hashedPassword
+    });
+
+    // generate a JWT token for user & send
+    const token = jwt.sign(
+      { id: userData._id, username },
+      process.env.SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+
+    userData.password = undefined;
+    res
+      .status(200)
+      .json({ message: "You have successfully reistered!", userData });
+  } catch (error) {
+    console.log("Error:" + error.message);
+  }
 });
 
 // login API
