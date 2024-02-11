a35916a9a7ca10689f12cd44c42aae9d399f0a68
srishtikapoor <srishtikapoor697@gmail.com>
2024-02-11 17:44:06 +0530

    added home page, sign-up page

--- AUTH/frontend/src/App.js
+++ AUTH/frontend/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import signup from "./SignUp.js";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header class="bg-white">
+        <nav
+          class="mx-auto flex max-w-7xl items-start justify-between p-6 lg:px-8"
+          aria-label="Global"
         >
-          Learn React
-        </a>
+          {/* Platform Logo */}
+          <div class="flex lg:flex-1">
+            <a Link to="/signup" class="-m-1.5 p-1.5">
+              <span class="sr-only">Your Company</span>
+              <img
+                class="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
+              />
+            </a>
+          </div>
+          <div class="hidden lg:flex lg:flex-1 lg:justify-end lg:gap-x-12">
+            <a
+              href="signup"
+              class="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Sign up
+            </a>
+            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">
+              Log in <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </nav>
       </header>
+      <h1 className="text-3xl font-bold">Welcome to Online Code Judge</h1>
     </div>
   );
 }
